1d9ccfbef5abac88532ec2e5cf346f5414d58aa9
hiruniherath0326 <hiruniherath0326@gmail.com>
2025-03-22 11:43:30 +0530

    Add files via upload

---
+++ MyThread/src/mythread/MyThread.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mythread;
+
+/**
+ *
+ * @author student
+ */
+public class MyThread extends Thread {
+    @Override
+public void run() {
+    System.out.println("Thread is running");
+}
+
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+        t.start();
+    }
+    
+}
